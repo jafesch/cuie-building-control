7b1445cb88429b2252ed497a5dfff800b0be2d0e
Julian Fisch <julian.fisch@asinfotrack.ch>
2017-01-11 14:46:18 +0100

    removed unnecessary files and added background highlighting rectangle

--- src/ch/fhnw/cuie/module04/buildings/singlebuilding/HeightSlider.java
+++ src/ch/fhnw/cuie/module04/buildings/singlebuilding/HeightSlider.java
@@ -2,18 +2,21 @@ package ch.fhnw.cuie.module04.buildings.singlebuilding;
 
 import ch.fhnw.cuie.module04.buildings.BuildingPM;
 import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.util.StringConverter;
 
 /**
  * Height slider which shows the height in relation to a building.
+ * @author Roman Fritschi, Julian Fisch
  */
 public class HeightSlider extends GridPane {
 	private final BuildingPM building;
@@ -44,12 +47,51 @@ public class HeightSlider extends GridPane {
 		getColumnConstraints().addAll(new ColumnConstraints(), cc);
 	}
 
+	/**
+	 * Sets up all parts used for the height slider.
+	 */
 	private void initializeParts() {
 		height_mLabel = new Label("Height (m)");
 		height_mField = new TextField();
 
-		buildingSlider = new  Slider(0, 1000, 100);
+		//stack pane which contains slider and rectangle as color overlay
+		sp = new StackPane();
+		sp.setAlignment(Pos.TOP_LEFT);
+		sp.setMaxWidth(200);
+
+		//rectangle to highlight selected area
+		Pane p = new Pane();
+		Rectangle rect = new Rectangle(30, 0, 170, 500);
+		rect.setFill(Color.BLACK);
+		rect.setOpacity(0.35);
+		p.getChildren().add(rect);
+
+		buildingSlider = new Slider(0, 1000, 100);
 		buildingSlider.getStylesheets().add(getClass().getResource("buildingSlider.css").toExternalForm());
+
+		sp.getStyleClass().add("my-stack-pane");
+		sp.getStylesheets().add(getClass().getResource("buildingSlider.css").toExternalForm());
+		sp.getChildren().addAll(p, buildingSlider);
+
+		//Adapt highlighting rectangle if slider value has changed
+		buildingSlider.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+								Number oldValue, Number newValue) {
+
+				if (newValue == oldValue) return;
+
+				int sliderPos = (int) ((1000 - (double) newValue) / 2);
+
+				//correction value to adapt rectangle to background position of the slider track element
+				sliderPos += (int) ((double)(newValue) - 500) / 50;
+
+				rect.setY(sliderPos);
+				rect.setHeight(500 - sliderPos);
+			}
+		});
+
+		buildingSlider.setValue(200);
 	}
 
 	private void layoutParts() {
@@ -58,7 +100,7 @@ public class HeightSlider extends GridPane {
 		valueGrid.setHgap(20);
 		valueGrid.setVgap(10);
 
-		this.addRow(0, valueGrid, buildingSlider);
+		this.addRow(0, valueGrid, sp);
 	}
 
 	private void setupBindings() {
@@ -79,6 +121,7 @@ public class HeightSlider extends GridPane {
 			}
 		};
 
+		//bind slider to input and vice versa
 		Bindings.bindBidirectional(height_mField.textProperty(), building.height_mProperty(), doubleStringConverter);
 		Bindings.bindBidirectional(building.height_mProperty(), buildingSlider.valueProperty());
 	}
